c0c686b88d841a016ea4500615426ae75c437863
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-06-26 10:20:49 +0200

    html-s changed to ejs without templating jet

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>home</p>')
-    res.sendFile('./views/index.html', { root: __dirname });
+    // res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
 // redirect
@@ -22,5 +27,5 @@ app.get('/about-us', (req, res) => {
 
 // 404 page - fire only if code reach this part
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404');
 });
