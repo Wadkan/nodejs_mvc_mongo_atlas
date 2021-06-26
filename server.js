aa3b7c0262827b9f24bb386badd10604f0c4aebd
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-06-26 07:42:50 +0200

    add status codes to the response

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ const server = http.createServer((req, res) => {
     switch(req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
@@ -27,7 +30,6 @@ const server = http.createServer((req, res) => {
             res.end();
         } else {
             // res.write(data);
-            // res.end();
             res.end(data);
         }
     })
