6e2f65cb07543f0833d072f35c4cdd7caf70756c
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-06-27 12:11:58 +0200

    added: save blogs to db

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // routes
@@ -37,9 +38,19 @@ app.get('/blogs', (req, res) => {
         .catch((err) => {
             console.log(err);
         })
+});
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 })
-
-
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create' });
