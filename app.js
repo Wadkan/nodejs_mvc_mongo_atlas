3d7eee5f35c2d5263ec864971e388697f8c4bd6e
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-06-27 11:11:18 +0200

    mongoDB atlas connetced

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
+
+// connect to mongoDB
+const dbURI = 'mongodb+srv://mongouser:iy!3kWCvcx4epmv@nodedemo.syaid.mongodb.net/node-demo-db?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true  })
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err));
+
+
 
 // register view engine
 app.set('view engine', 'ejs');
